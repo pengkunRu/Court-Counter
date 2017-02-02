7a6180af7fb0fb2f0cd3350cd13fdb10f1d80871
rupengkun <522523828@qq.com>
2017-02-02 22:35:39 +0800

    1.03 I click the plus 3 points,3 is shown, I click plus 2 points,2 is shown, and I click free-throw and one is shown

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * the method is called when +3 button is pressed
+     */
+    public void addThreeForTeamA(View view){
+        displayForTeamA(3);
+    }
+
+    /**
+     * the method is called when +2 button is pressed
+     */
+    public void addTwoForTeamA(View view){
+        displayForTeamA(2);
+    }
+
+    /**
+     * the method is called when +1 button is pressed
+     */
+    public void addOneForTeamA(View view){
+        displayForTeamA(1);
+    }
+
+    /**
+     * Display the given score for team A
+     */
+    private void displayForTeamA(int score){
+        TextView scoreView = (TextView)findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
